d43dfaa4736fc6e8b60366965f8bee939ddeb461
Antonnio Jones <antonnioj@gmail.com>
2020-02-16 02:47:33 -0500

    navigation bar update

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("ok")
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div id="app">
+        <NavBar />
+        <HomeSection />
+      </div>
+    )
+  }
+}
+
+const NavBar = (props) => {
+  return (
+  <nav id="nav-section">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  )
+}
+
+const HomeSection = () =>{
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
