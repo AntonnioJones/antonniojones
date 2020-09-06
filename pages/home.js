cc6c66e5d81fb55a6092b023646a8d39910cbcb1
Antonnio Jones <antonnioj@gmail.com>
2020-09-06 19:46:46 -0400

    fixed the routes

--- pages/home.js
+++ pages/home.js
@@ -9,12 +9,10 @@ class Home extends React.Component{
    render(){
    return (
      <div id="homeSection" className="col align-self-center">
-       <FadeInSection pose={this.state.isVisible ? 'visible' : 'hidden'}>
          <h1>Welcome to my portfolio</h1>
          <p>I am a software developer that specalizes in front end development.</p>
          <p>I enjoy game development, building web applications, and learning new skills</p>
          <p>Scroll down to checkout some of my work, view my skills, or to contact me</p>
-       </FadeInSection>
      </div>
    )
    }
