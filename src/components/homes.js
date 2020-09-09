dd7271ac37e977a9cb1170682a89739b77dba919
Antonnio Jones <antonnioj@gmail.com>
2020-09-09 17:57:27 -0400

    added some animation

--- src/components/homes.js
+++ src/components/homes.js
@@ -1,16 +1,20 @@
 import React from "react";
 import theme from "../util/theme";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
 
 //material UI
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 
 const useStyles = makeStyles({
-    container: {
+  container: {
       paddingTop: 20,
       textAlign: "center",
+      backgroundColor: "black",
+      color:"white",
+      height: "100"
     },
 
 
@@ -23,8 +27,9 @@ function Home() {
         <h1>Antonnio Jones</h1>
         <h3>A Full Stack Developer</h3>
         <Button variant="outlined" color="primary">
-          Learn More
+          Learn More <ArrowDownwardIcon> </ArrowDownwardIcon>
         </Button>
+        
      </Grid>
   );
 }
