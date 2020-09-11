6c9341e2472da23a6935f6ae8e4bfe533d313561
Antonnio Jones <antonnioj@gmail.com>
2020-09-11 17:41:03 -0400

    2

--- src/components/about.js
+++ src/components/about.js
@@ -1,32 +1,52 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 //material UI
-import Grid from '@material-ui/core/Grid'
-import { Container } from '@material-ui/core';
+import Grid from "@material-ui/core/Grid";
+import { Container } from "@material-ui/core";
+import { FullscreenExit } from "@material-ui/icons";
 
 const useStyles = makeStyles({
-    container: {
-      backgroundColor: "#F1F1F1",
-      paddingTop: 20,
-      width: "100%"
-    },
-
-
+  root: {
+    backgroundColor: "#F1F1F1",
+    paddingTop: 20,
+    textAlign: "center",
+    minHeight: "100vh",
+  },
+  about: {
+    paddingLeft: 20,
+    textAlign: "left"
+  }
 });
 
 const About = () => {
-    const classes = useStyles();
-    return (
-      <Grid className={classes.container}>
-            <Container>
-              <h1>Profile</h1>
-            </Container>
-            <Container>
-              <h1>Skills</h1>
-            </Container>
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      className={classes.root}
+      direction="row"
+      justify="space-evenly"
+      alignItems="flex-start"
+    >
+      <Grid container item xs={6}>
+        <div className={classes.about}>
+          <h1>About</h1>
+          <img src="../images/no-img.png" alt="Profile Picture"></img>
+          <div>
+            <h4>Education: </h4>
+            <p>Coastal Carolina University: bachlor of science(Computer Science)</p>
+          </div>
+          <p id="about info"></p>
+        </div>
       </Grid>
-    )
-}
+      <Grid container item xs={6}>
+        <div className="about">
+          <h1>Skills</h1>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default About;
